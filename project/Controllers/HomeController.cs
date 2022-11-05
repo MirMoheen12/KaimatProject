c33c02072c5e47d9a0a3608399194515eb7a5c2e
Mir Moheen <mirmoheen9@gmail.com>
2022-11-05 22:47:54 +0500

    setup project

---
+++ project/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace project.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+ 
+
+
+
+        public ActionResult about()
+        {
+            return View();
+        }
+        public ActionResult contact()
+        {
+            return View();
+        }
+        public ActionResult pricing()
+        {
+            return View();
+        }
+        public ActionResult project()
+        {
+            return View();
+        }
+        public ActionResult service()
+        {
+            return View();
+
+
+        }
+        public ActionResult projectdetails()
+        {
+            return View();
+        }
+
+       public ActionResult blogsidebar()
+        {
+            return View();
+        }
+        public ActionResult blogsingle()
+        {
+            return View();
+        }
+    }
+}
